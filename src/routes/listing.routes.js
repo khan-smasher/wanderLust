42e5ef6772487e39bd024ac0f488fb753f37d8f8
Mohammad Sulaiman Khan <mohammadkhan0829@gmail.com>
2025-07-17 13:08:36 +0530

    fixes

--- src/routes/listing.routes.js
+++ src/routes/listing.routes.js
@@ -11,10 +11,11 @@ const router = Router();
 
 router.route("/").get(showAllListings);
 router.route("/new").get(renderFormForNewListing);
-router.route("/create").post(createNewListing);
+router.route("/").post(createNewListing); // form posts to "/"
+
 router.route("/:id/edit").get(renderEditForm);
-router.route("/:id/delete").get(deleteListing);
-router.route("/:id").patch(updateListing);
 router.route("/:id").get(showIndividualListing);
+router.route("/:id").patch(updateListing);
+router.route("/:id").delete(deleteListing);
 
 export default router;
