a72d980e1813a6f44b40c55b97bdc8af7fd9f8c5
Mohammad Sulaiman Khan <mohammadkhan0829@gmail.com>
2025-07-17 13:12:27 +0530

    controller fixing

--- src/controllers/listing.controller.js
+++ src/controllers/listing.controller.js
@@ -4,67 +4,66 @@ import Listing from "../models/listing.model.js";
 
 const showAllListings = asyncHandler(async (req, res) => {
   const allListings = await Listing.find({});
-    res.render("listings/index", {allListings})
+  res.render("listings/index", { allListings });
 });
 
 const showIndividualListing = asyncHandler(async (req, res) => {
-    const {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show", {listing})
-
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show", { listing });
 });
 
 const renderFormForNewListing = asyncHandler(async (req, res) => {
-    res.render("listings/new")
-})
+  res.render("listings/new");
+});
 
 const createNewListing = asyncHandler(async (req, res) => {
-    const { title, description, image, price, location, country } = req.body;
+  const { title, description, image, price, location, country } = req.body;
 
-    const newListing = await Listing.create({
-        title,
-        description,
-        image,
-        price,
-        location,
-        country
-    });
+  const newListing = await Listing.create({
+    title,
+    description,
+    image,
+    price,
+    location,
+    country,
+  });
 
-    res.redirect(`/api/v1/listings/${newListing._id}`);
-})
+  res.redirect(`/api/v1/listings/${newListing._id}`);
+});
 
 const renderEditForm = asyncHandler(async (req, res) => {
-    const {id} = req.params;
-    const listing = await Listing.findById(id);
-   res.render("listings/edit", { listing }) 
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit", { listing });
 });
 
 const updateListing = asyncHandler(async (req, res) => {
   const { id } = req.params;
 
-  const updatedListing = await Listing.findByIdAndUpdate(
-    id,
-    req.body,
-    { new: true, runValidators: true }
-  );
+  const updatedListing = await Listing.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
   res.redirect(`/api/v1/listings/${updatedListing._id}`);
 });
 
 const deleteListing = asyncHandler(async (req, res) => {
-    const {id} = req.params;
-    console.log(`This listing is going to be deleted: \n${Listing.findById(id)}`);
-    await Listing.findByIdAndDelete(id);
-    console.log("Successfully Deleted");
-    res.redirect(`/api/v1/listings/`);
-})
+  const { id } = req.params;
+  const listingToDelete = await Listing.findById(id);
+  console.log(`This listing is going to be deleted:\n`, listingToDelete);
+  await Listing.findByIdAndDelete(id);
+  console.log("Successfully Deleted");
+  res.redirect(`/api/v1/listings/`);
+});
 
-export { 
-    showAllListings,
-    showIndividualListing,
-    renderFormForNewListing,
-    createNewListing,
-    renderEditForm,
-    updateListing,
-    deleteListing
- };
+export {
+  showAllListings,
+  showIndividualListing,
+  renderFormForNewListing,
+  createNewListing,
+  renderEditForm,
+  updateListing,
+  deleteListing,
+};
